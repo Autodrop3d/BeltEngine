21cff584a17a1d52684316014f6c0c4f22aa7b18
mmiscool <github@smbisoft.com>
2020-11-16 23:53:23 -0500

    getting setup script for raspbery pi working

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt update
+
+
+sudo apt install python3
+sudo apt install libspatialindex-dev -y
+sudo apt install libgeos-dev -y
+sudo apt install python3-numpy -y
+sudo apt install libatlas3-base -y
+sudo apt install pip pip3 -y
+sudo apt install python3 python-numpy python3-numpy -y
+sudo apt install python3-scipy -y
+
+pip install numpy
+pip3 install numpy
+
+sudo pip3 install -r requirements.txt
+
